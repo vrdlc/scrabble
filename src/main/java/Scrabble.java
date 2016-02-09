c4c21e3d8f4d1b2affded4978389cb6876d0aef6
Austin Minnon and Ramon de la Cruz <whi7ee@gmail.com>
2016-02-09 11:03:50 -0800

    first spec passed

--- src/main/java/Scrabble.java
+++ src/main/java/Scrabble.java
@@ -1,8 +1,22 @@
-public class Scrabble {
-  public static void main(String[] args) {}
+import java.io.Console;
 
-  public static Integer scrabbleScore(String word) {
+public class Scrabble {
+  public static void main(String[] args) {
+  // Console myConsole = System.console();
+  //
+  // System.out.println("Give me a word.");
+  // String userWord = myConsole.readLine();
+}
+  public static Integer scrabbleScore(String userWord) {
+
+    char[] scrabbleArray = userWord.toCharArray();
     Integer score = 0;
+    for ( char letter : scrabbleArray) {
+      if ( letter == 'a') {
+        score += 1;
+      }
+    }
+    System.out.println();
     return score;
   }
-  }
+}
