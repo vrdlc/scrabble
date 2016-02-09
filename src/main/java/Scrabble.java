3a91abcee31118a3356f241371b48e43f079907f
Austin Minnon and Ramon de la Cruz <virgilioramon.delacruz@gmail.com>
2016-02-09 15:09:49 -0800

    scrabble score works but runs very slowly

--- src/main/java/Scrabble.java
+++ src/main/java/Scrabble.java
@@ -1,20 +1,40 @@
-import java.io.Console;
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
 
 public class Scrabble {
   public static void main(String[] args) {
-  // Console myConsole = System.console();
-  //
-  // System.out.println("Give me a word.");
-  // String userWord = myConsole.readLine();
-}
+  String layout = "templates/layout.vtl";
+
+  get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/home.vtl");
+
+      return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
+
+  get("/detector", (request, response) -> {
+     Map<String, Object> model = new HashMap<String, Object>();
+     model.put("template", "templates/detector.vtl");
+
+     String userWord = request.queryParams("userWord");
+    int score = scrabbleScore(userWord);
+
+     model.put("userWord", score);
+     return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
+  }
+
+
   public static Integer scrabbleScore(String userWord) {
 
     char[] scrabbleArray = userWord.toCharArray();
-    Integer score = 0;
+    int score = 0;
     for ( char letter : scrabbleArray) {
-      if ( letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' || letter == 'r' || letter == 'l' || letter == 'n' || letter == 's' || letter == 't') {
-        score += 1;
-      } else if (letter == 'd' || letter == 'g'){
+      if (letter == 'd' || letter == 'g'){
         score += 2;
       } else if (letter == 'b' || letter == 'c' || letter == 'm' || letter == 'p') {
         score += 3;
@@ -26,9 +46,11 @@ public class Scrabble {
         score += 8;
       } else if (letter == 'q' || letter == 'z') {
         score += 10;
+      } else {
+        score += 1;
       }
     }
-    System.out.println();
+
     return score;
 
   }
--- src/test/java/ScrabbleTest.java
+++ src/test/java/ScrabbleTest.java
@@ -13,25 +13,25 @@ public class ScrabbleTest {
   public void scrabbleScore_returnsAScrabbleScoreForValue1_9() {
     Scrabble scrabble = new Scrabble();
     Integer score = 9;
-    assertEquals(score, scrabble.scrabbleScore("e,i,o,u,l,n,r,s,t"));
+    assertEquals(score, scrabble.scrabbleScore("eioulnrst"));
   }
   @Test
   public void scrabbleScore_returnsAScrabbleScoreForValue2_4() {
     Scrabble scrabble = new Scrabble();
     Integer score = 4;
-    assertEquals(score, scrabble.scrabbleScore("d,g"));
+    assertEquals(score, scrabble.scrabbleScore("dg"));
   }
   @Test
   public void scrabbleScore_returnsAScrabbleScoreForValue3_12() {
     Scrabble scrabble = new Scrabble();
     Integer score = 12;
-    assertEquals(score, scrabble.scrabbleScore("b,c,m,p"));
+    assertEquals(score, scrabble.scrabbleScore("bcmp"));
   }
   @Test
   public void scrabbleScore_returnsAScrabbleScoreForValue4_20() {
     Scrabble scrabble = new Scrabble();
     Integer score = 20;
-    assertEquals(score, scrabble.scrabbleScore("f, h, v, w, y"));
+    assertEquals(score, scrabble.scrabbleScore("fhvwy"));
   }
   @Test
   public void scrabbleScore_returnsAScrabbleScoreForValue5_5() {
@@ -43,12 +43,18 @@ public class ScrabbleTest {
   public void scrabbleScore_returnsAScrabbleScoreForValue8_16() {
     Scrabble scrabble = new Scrabble();
     Integer score = 16;
-    assertEquals(score, scrabble.scrabbleScore("j, x"));
+    assertEquals(score, scrabble.scrabbleScore("jx"));
   }
   @Test
   public void scrabbleScore_returnsAScrabbleScoreForValue10_20() {
     Scrabble scrabble = new Scrabble();
     Integer score = 20;
-    assertEquals(score, scrabble.scrabbleScore("q, z"));
+    assertEquals(score, scrabble.scrabbleScore("qz"));
+  }
+  @Test
+  public void scrabbleScore_returnsAScrabbleScoreForTestWord_38() {
+    Scrabble scrabble = new Scrabble();
+    Integer score = 38;
+    assertEquals(score, scrabble.scrabbleScore("quizzical"));
   }
 }
