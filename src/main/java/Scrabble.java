c9764c2c84e80684a7555da5d17d6c1d4cf4e132
Austin Minnon and Ramon de la Cruz <whi7ee@gmail.com>
2016-02-09 08:50:51 -0800

    initalized files and folders

---
+++ src/main/java/Scrabble.java
@@ -0,0 +1,8 @@
+public class Scrabble {
+  public static void main(String[] args) {}
+
+  public Boolean scrabbleScore(String word) {
+    // eventually put your code here
+    return false;
+  }
+  }
---
+++ src/test/java/ScrabbleTest.java
@@ -0,0 +1,9 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class ScrabbleTest {
+
+  @Test
+  public void scrabbleScore_returnsAScrabbleScoreForALetter_1() {
+  }
+}
